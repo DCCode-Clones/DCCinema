55bc59b1e358a55c0ff64eb57264d37c9d4ec88b
chris-carstens <ctcarstens@uc.cl>
2021-11-11 22:30:23 -0300

    A~

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,15 +8,23 @@ class MainController < ApplicationController
     end
 
     def day
-        day = params[:day]
+        @selected_day = params[:day]
         @movies = Movie.all()
         @schedules = Schedule.all()
-        if day
-            @movies = Movie.where("start_date <= ? AND end_date >= ?", day, day)
+        if @selected_day
+            @movies = Movie.where("start_date <= ? AND end_date >= ?", @selected_day, @selected_day)
         end
         start_day = @movies.map {|movie| movie.start_date }.sort.first
         end_day = @movies.map {|movie| movie.end_date }.sort.last
         @days = (start_day..end_day).to_a
+        @schedules_matine = Schedule.where("day = ? AND time = ?", @selected_day, 'matine')
+        @schedules_tanda = Schedule.where("day = ? AND time = ?", @selected_day, 'tanda')
+        @schedules_noche = Schedule.where("day = ? AND time = ?", @selected_day, 'noche')
+    end
+
+    def rooms
+        day = params[:day]
+
     end
 
 end
