24bd1d6021fbdfd8043c21d1fbe058162a48d2c2
Christian Carstens <42195947+chris-carstens@users.noreply.github.com>
2021-11-14 19:32:52 -0300

    Create movies (#13)
    * feat(common): create movies
    
    * Add creacte movie
    
    Co-authored-by: msmendoza <msmendoza@uc.cl>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,26 +2,25 @@ class MainController < ApplicationController
     def index
         @movies = Movie.all
         @schedules = Schedule.all
+        @current_date = Date.current
 
         unless @movies.empty?
           start_day = @movies.map { |movie| movie.start_date }.sort.first
           end_day = @movies.map { |movie| movie.end_date }.sort.last
           @days = (start_day..end_day).to_a
-        end
-    end
 
-    def day
-        @selected_day = params[:day]
-        @movies = Movie.all
-        @schedules = Schedule.all
-        if @selected_day
+          @selected_day = params[:day]
+          if @selected_day
             @movies = Movie.where("start_date <= ? AND end_date >= ?", @selected_day, @selected_day)
+            @schedules_matine = Schedule.where("day = ? AND time = ?", @selected_day, 'matine')
+            @schedules_tanda = Schedule.where("day = ? AND time = ?", @selected_day, 'tanda')
+            @schedules_noche = Schedule.where("day = ? AND time = ?", @selected_day, 'noche')
+          else
+            @movies = Movie.where("start_date <= ? AND end_date >= ?", @current_date, @current_date)
+            @schedules_matine = Schedule.where("day = ? AND time = ?", @current_date, 'matine')
+            @schedules_tanda = Schedule.where("day = ? AND time = ?", @current_date, 'tanda')
+            @schedules_noche = Schedule.where("day = ? AND time = ?", @current_date, 'noche')
+          end
         end
-        start_day = @movies.map {|movie| movie.start_date }.sort.first
-        end_day = @movies.map {|movie| movie.end_date }.sort.last
-        @days = (start_day..end_day).to_a
-        @schedules_matine = Schedule.where("day = ? AND time = ?", @selected_day, 'matine')
-        @schedules_tanda = Schedule.where("day = ? AND time = ?", @selected_day, 'tanda')
-        @schedules_noche = Schedule.where("day = ? AND time = ?", @selected_day, 'noche')
     end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,16 @@
 class MoviesController < ApplicationController
+  def new
+  end
+
+  def create
+    new_movie = Movie.create(movie_params)
+    new_movie.save()
+    redirect_to '/'
+  end
+
+  private
+
+  def movie_params
+    params.permit(:name, :imgURL, :start_date, :end_date)
+  end
 end
