4887995e4d1ae085330902e02b9fe0735261d490
rgmorales1 <rgmorales1@uc.cl>
2021-11-17 18:47:59 -0300

    Add reserve function with checkbox form error

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,25 @@
 class SchedulesController < ApplicationController
-    def show
-        @schedule = Schedule.find_by(id: params[:id])
-        @movie = @schedule.movie
+  def show
+    @schedule = Schedule.find_by(id: params[:id])
+    @movie = @schedule.movie
+  end
+
+  def reserve
+    movie_params
+    puts(params["seats"])
+    @schedule = Schedule.find_by(id: params[:id])
+    for i in 0..11 do
+      if params["seats"][i] == "1"
+        @schedule.seats[params["row"].to_i][i] = 1 
+      end
     end
+    @schedule.save
+    redirect_to "/schedules/#{@schedule.id}"
+  end
+
+  private
+  
+  def movie_params
+    params.permit(:row, :seats)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,5 @@ movies = Movie.create([{ name: 'Star Wars', imgURL: "https://i.blogs.es/26ba45/s
                     { name: 'Los Simpsons', imgURL: "https://phantom-marca.unidadeditorial.es/ff129bbe4e38ec6dd131e989f544d6bf/resize/1320/f/jpg/assets/multimedia/imagenes/2021/03/04/16148576621458.jpg", start_date: Date.current, end_date: Date.tomorrow },
                     { name: 'Spiderman', imgURL: "https://upload.wikimedia.org/wikipedia/commons/9/90/Spiderman.JPG", start_date: Date.current, end_date: Date.tomorrow }])
 
-schedules = Schedule.create([{ time: "matine", room: 1, seats: Array.new(48) { 0 }, day: Date.current, movie: movies.first },
-                            { time: "matine", room: 2, seats: Array.new(48) { 0 }, day: Date.tomorrow, movie: movies.first }])
+schedules = Schedule.create([{ time: "matine", room: 1, seats: Array.new(4) { Array.new(12) {0} }, day: Date.current, movie: movies.first },
+                            { time: "matine", room: 2, seats: Array.new(4) { Array.new(12) {0} }, day: Date.tomorrow, movie: movies.first }])
