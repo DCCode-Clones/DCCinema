69d2baf8c64c2b6bb7f736eb69c91602d900d823
Trinidad Vargas <mtvargas1@uc.cl>
2021-11-18 16:55:24 -0300

    seats validation

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -7,17 +7,36 @@ class SchedulesController < ApplicationController
   def reserve
     movie_params
     puts(params["seats"])
-    @schedule = Schedule.find_by(id: params[:id])
-    params["seats"].each do |seat|
-      @schedule.seats[params["row"].to_i][seat.to_i] = 1
+    if !params["row"] || !params["seats"]
+      puts "no row or no seat!!!"
+    else
+      @schedule = Schedule.find_by(id: params[:id])
+      @occupied_seats = Array.new
+      @reserved = Array.new
+      params["seats"].each do |seat|
+        if @schedule.seats[params["row"].to_i][seat.to_i] == 1
+          @occupied_seats.push(seat)
+        else
+          @schedule.seats[params["row"].to_i][seat.to_i] = 1
+          @reserved.push(seat)
+        end
+      end
+      puts "@@", @reserved, "--", @occupied_seats
+      @schedule.save
+      redirect_to "/schedules/#{@schedule.id}#row=#{row_n_to_let(params[:row])}"
     end
-    @schedule.save
-    redirect_to "/schedules/#{@schedule.id}"
+
+    render :show
+    
   end
 
   private
   
   def movie_params
     params.permit(:row, :seats)
+  end
+
+  def row_n_to_let(row)
+    return {0 => :A, 1 => :B, 2 => :C, 3 => :D}[row.to_i]
   end
 end
