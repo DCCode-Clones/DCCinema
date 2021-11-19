cdfc863efa1d7a980a050082c12b48d655bbf350
tomascortes <ticortes@uc.cl>
2021-11-19 12:12:59 -0300

    feat(validations scheduling seats)

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,13 +2,23 @@ class SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find_by(id: params[:id])
     @movie = @schedule.movie
+    @reservation_error =  session[:reservation_error] 
   end
 
   def reserve
     movie_params
-    puts(params["seats"])
     @schedule = Schedule.find_by(id: params[:id])
+    session[:reservation_error] = false
+    if !params["seats"]
+      redirect_to "/schedules/#{@schedule.id}"  and return
+    end
     params["seats"].each do |seat|
+      if @schedule.seats[params["row"].to_i][seat.to_i] == 1
+        session[:reservation_error] = true
+        redirect_to "/schedules/#{@schedule.id}"  and return
+      else
+        session[:reservation_error] = false
+      end
       @schedule.seats[params["row"].to_i][seat.to_i] = 1
     end
     @schedule.save
