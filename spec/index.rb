3c6694ed01d3ff8ed850613c1b467ba6c60d2462
tomascortes <ticortes@uc.cl>
2021-11-22 15:43:03 -0300

    feat(test env seted up)

--- spec/index.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Welcome' , type: :system do
-    it 'index' do
-        visit '/'
-        expec(page).to have_content('Welcome to the DCCinema')
-    end
-end
